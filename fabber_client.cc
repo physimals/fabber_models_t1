70ea61c314677bdc26ac6479d27970ef39a70f77
Jesper Kallehauge <jesper.kallehauge@eng.ox.ac.uk>
2016-02-01 12:44:52 +0000

    initial commit

---
+++ fabber_client.cc
@@ -0,0 +1,20 @@
+/*  FABBER - Fast T1 Bayesian Estimation Routine
+
+    Jesper Kallehauge, IBME
+
+    Copyright (C) 2007-2016 University of Oxford  */
+
+/*  CCOPYRIGHT */
+
+#include "fabbercore/fabber_core.h"
+
+// DWI models to be included from library
+#include "fwdmodel_VFA.h"
+int main(int argc, char** argv) {
+
+  //add the T1 models - these will autoregister at this point
+  VFAFwdModel::NewInstance();
+  
+  return execute(argc, argv);
+
+}
