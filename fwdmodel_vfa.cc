7c66a05573b837fedd88d961fe580bac298b0694
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-12-07 10:32:35 +0000

    Formatted

--- fwdmodel_vfa.cc
+++ fwdmodel_vfa.cc
@@ -12,19 +12,20 @@
 
 #include <newmatio.h>
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace NEWMAT;
 using namespace std;
 
-FactoryRegistration<FwdModelFactory, VFAFwdModel>
-  VFAFwdModel::registration("vfa");
+FactoryRegistration<FwdModelFactory, VFAFwdModel> VFAFwdModel::registration("vfa");
 
 static OptionSpec OPTIONS[] = {
     { "tr", OPT_FLOAT, "TR for VFA images", OPT_REQ, "" },
     { "fas-file", OPT_MATRIX, "File containing a list of flip angles", OPT_NONREQ, "" },
-    { "fa<n>", OPT_MATRIX, "Alternative to fas-file, specify a sequence of flip angles --fa1=30 --fa2=38 etc", OPT_NONREQ, "" },
+    { "fa<n>", OPT_MATRIX,
+        "Alternative to fas-file, specify a sequence of flip angles --fa1=30 --fa2=38 etc",
+        OPT_NONREQ, "" },
     { "radians", OPT_BOOL, "If specified, flip angles are given in radians", OPT_NONREQ, "" },
 };
 
@@ -38,7 +39,8 @@ void VFAFwdModel::GetOptions(vector<OptionSpec> &opts) const
 
 std::string VFAFwdModel::GetDescription() const
 {
-    return "Calculates T1 from variable flip angle data, using the SPGR equation assuming a fixed TR and that TE<<T2*";
+    return "Calculates T1 from variable flip angle data, using the SPGR equation assuming a fixed "
+           "TR and that TE<<T2*";
 }
 
 string VFAFwdModel::ModelVersion() const
@@ -55,46 +57,50 @@ string VFAFwdModel::ModelVersion() const
 
 void VFAFwdModel::Initialize(FabberRunData &rundata)
 {
-  m_tr = rundata.GetDouble("tr");
-  string fas_file = rundata.GetStringDefault("fas-file", "");
-  if (fas_file != "") {
-      m_fas = fabber::read_matrix_file(fas_file);
-  }
-  vector<double> fas = rundata.GetDoubleList("fa");
-  if (fas.size() > 0) {
-    if (fas_file != "") {
-      throw FabberRunDataError("Can't specify flip angles in a file and also using individual options");
+    m_tr = rundata.GetDouble("tr");
+    string fas_file = rundata.GetStringDefault("fas-file", "");
+    if (fas_file != "")
+    {
+        m_fas = fabber::read_matrix_file(fas_file);
     }
-    m_fas.ReSize(fas.size());
-    for (unsigned int i=0; i<fas.size(); i++) {
-      m_fas(i+1) = fas[i];
+    vector<double> fas = rundata.GetDoubleList("fa");
+    if (fas.size() > 0)
+    {
+        if (fas_file != "")
+        {
+            throw FabberRunDataError(
+                "Can't specify flip angles in a file and also using individual options");
+        }
+        m_fas.ReSize(fas.size());
+        for (unsigned int i = 0; i < fas.size(); i++)
+        {
+            m_fas(i + 1) = fas[i];
+        }
     }
-  }
-  m_radians = rundata.GetBool("radians");
+    m_radians = rundata.GetBool("radians");
 }
 
-void VFAFwdModel::NameParams(vector<string>& names) const
+void VFAFwdModel::NameParams(vector<string> &names) const
 {
-  names.clear();
+    names.clear();
 
-  names.push_back("T1");
-  names.push_back("sig0");
+    names.push_back("T1");
+    names.push_back("sig0");
 }
 
-void VFAFwdModel::HardcodedInitialDists(MVNDist& prior,
-    MVNDist& posterior) const
+void VFAFwdModel::HardcodedInitialDists(MVNDist &prior, MVNDist &posterior) const
 {
     assert(prior.means.Nrows() == NumParams());
 
-     SymmetricMatrix precisions = IdentityMatrix(NumParams()) * 1e-12;
+    SymmetricMatrix precisions = IdentityMatrix(NumParams()) * 1e-12;
 
     // Set priors
     // Fp or Ktrans whatever you belive
-     prior.means(T1_index()) = 0.01;
-     precisions(T1_index(),T1_index()) = 1e-12;
+    prior.means(T1_index()) = 0.01;
+    precisions(T1_index(), T1_index()) = 1e-12;
 
-     prior.means(sig0_index()) = 0.01;
-     precisions(sig0_index(),sig0_index()) = 1e-12;
+    prior.means(sig0_index()) = 0.01;
+    precisions(sig0_index(), sig0_index()) = 1e-12;
 
     // Set precsions on priors
     prior.SetPrecisions(precisions);
@@ -105,82 +111,80 @@ void VFAFwdModel::HardcodedInitialDists(MVNDist& prior,
     // For parameters with uniformative prior chosoe more sensible inital posterior
     // Tissue perfusion
     posterior.means(T1_index()) = 1;
-    precisions(T1_index(),T1_index()) = 0.1;
+    precisions(T1_index(), T1_index()) = 0.1;
 
     posterior.SetPrecisions(precisions);
 }
 
-void VFAFwdModel::Evaluate(const ColumnVector& params, ColumnVector& result) const
+void VFAFwdModel::Evaluate(const ColumnVector &params, ColumnVector &result) const
 {
     // ensure that values are reasonable
     // negative check
-   ColumnVector paramcpy = params;
-    for (int i=1;i<=NumParams();i++) {
-      if (params(i)<0) { paramcpy(i) = 0; }
-      }
+    ColumnVector paramcpy = params;
+    for (int i = 1; i <= NumParams(); i++)
+    {
+        if (params(i) < 0)
+        {
+            paramcpy(i) = 0;
+        }
+    }
 
-   // parameters that are inferred - extract and give sensible names
-   float T1;
-   float sig0; //'inital' value of the signal
-   ColumnVector FA_radians;
+    // parameters that are inferred - extract and give sensible names
+    float T1;
+    float sig0; //'inital' value of the signal
+    ColumnVector FA_radians;
 
-   // extract values from params
-   sig0 = paramcpy(sig0_index());
-   T1 = paramcpy(T1_index());
-   if (T1<1e-8) T1=1e-8;
-   if (sig0<1e-8) sig0=1e-8;
+    // extract values from params
+    sig0 = paramcpy(sig0_index());
+    T1 = paramcpy(T1_index());
+    if (T1 < 1e-8)
+        T1 = 1e-8;
+    if (sig0 < 1e-8)
+        sig0 = 1e-8;
 
-   ColumnVector m_fashere; // the arterial signal to use for the analysis
-   if (m_fas.Nrows()>0) {
-     m_fashere = m_fas; //use the artsig that was loaded by the model
-   }
-   else {
-     //use an artsig from supplementary data
-     if (suppdata.Nrows()>0) {
-       m_fashere = suppdata;
-     }
-     else {
-       cout << "No valid b values found" << endl;
-       throw;
-     }
-   }
-   // use length of the aif to determine the number of time points
-   int ntpts = m_fashere.Nrows();
-   FA_radians=m_fas*3.1415926/180;
+   
+    int ntpts = m_fas.Nrows();
+    FA_radians = m_fas * 3.1415926 / 180;
 
+    // --- SPGR Function ----
+    ColumnVector sig(ntpts);
+    sig = 0.0;
 
+    if (m_radians)
+    {
+        for (int i = 1; i <= ntpts; i++)
+        {
+            sig(i) = sig0 * sin(m_fas(i)) * (1 - exp(-m_tr / T1))
+                / (1 - cos(m_fas(i)) * exp(-m_tr / T1));
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= ntpts; i++)
+        {
+            sig(i) = sig0 * sin(FA_radians(i)) * (1 - exp(-m_tr / T1))
+                / (1 - cos(FA_radians(i)) * exp(-m_tr / T1));
+        }
+    }
 
-   // --- SPGR Function ----
-   ColumnVector sig(ntpts);
-   sig=0.0;
+    result = sig;
 
-   if (m_radians){
-       for (int i=1; i<=ntpts; i++){
-       sig(i) = sig0*sin(m_fas(i))*(1-exp(-m_tr/T1))/(1-cos(m_fas(i))*exp(-m_tr/T1));
-       }
-   }
-       else{
-       for (int i=1; i<=ntpts; i++){
-       sig(i) = sig0*sin(FA_radians(i))*(1-exp(-m_tr/T1))/(1-cos(FA_radians(i))*exp(-m_tr/T1));
-       }
-   }
+    for (int i = 1; i <= ntpts; i++)
+    {
+        if (isnan(result(i)) || isinf(result(i)))
+        {
+            exit(0);
+            LOG << "Warning NaN of inf in result" << endl;
+            LOG << "result: " << result.t() << endl;
+            LOG << "params: " << params.t() << endl;
 
-   result=sig;
-
-   for (int i=1; i<=ntpts; i++) {
-     if (isnan(result(i)) || isinf(result(i))) {
-       exit(0);
-       LOG << "Warning NaN of inf in result" << endl;
-       LOG << "result: " << result.t() << endl;
-       LOG << "params: " << params.t() << endl;
-
-       result=0.0;
-       break;
-         }
-   }
+            result = 0.0;
+            break;
+        }
+    }
 }
 
-FwdModel* VFAFwdModel::NewInstance()
+FwdModel *VFAFwdModel::NewInstance()
 {
-  return new VFAFwdModel();
+    return new VFAFwdModel();
 }
--- fwdmodel_vfa.h
+++ fwdmodel_vfa.h
@@ -13,33 +13,45 @@
 #include <string>
 #include <vector>
 
-class VFAFwdModel : public FwdModel {
+class VFAFwdModel : public FwdModel
+{
 public:
-  static FwdModel* NewInstance();
+    static FwdModel *NewInstance();
 
-  virtual void Initialize(FabberRunData &rundata);
-  virtual std::string ModelVersion() const;
-  virtual void GetOptions(std::vector<OptionSpec> &opts) const;
-  virtual std::string GetDescription() const;
+    virtual void Initialize(FabberRunData &rundata);
+    virtual std::string ModelVersion() const;
+    virtual void GetOptions(std::vector<OptionSpec> &opts) const;
+    virtual std::string GetDescription() const;
 
-  virtual void NameParams(std::vector<std::string>& names) const;
-  virtual int NumParams() const { return 2; }
+    virtual void NameParams(std::vector<std::string> &names) const;
+    virtual int NumParams() const
+    {
+        return 2;
+    }
 
-  virtual void HardcodedInitialDists(MVNDist& prior, MVNDist& posterior) const;
-  virtual void Evaluate(const NEWMAT::ColumnVector& params, NEWMAT::ColumnVector& result) const;
+    virtual void HardcodedInitialDists(MVNDist &prior, MVNDist &posterior) const;
+    virtual void Evaluate(const NEWMAT::ColumnVector &params, NEWMAT::ColumnVector &result) const;
 
-  virtual ~VFAFwdModel() {}
+    virtual ~VFAFwdModel()
+    {
+    }
 
 protected:
-  // Lookup the starting indices of the parameters
-  int T1_index() const {return 1;}
-  int sig0_index() const { return 2; }
+    // Lookup the starting indices of the parameters
+    int T1_index() const
+    {
+        return 1;
+    }
+    int sig0_index() const
+    {
+        return 2;
+    }
 
-  double m_tr;
-  bool m_radians;
-  NEWMAT::ColumnVector m_fas;
+    double m_tr;
+    bool m_radians;
+    NEWMAT::ColumnVector m_fas;
 
 private:
-  /** Auto-register with forward model factory. */
-  static FactoryRegistration<FwdModelFactory, VFAFwdModel> registration;
+    /** Auto-register with forward model factory. */
+    static FactoryRegistration<FwdModelFactory, VFAFwdModel> registration;
 };
